aab80ed5c5cd65d081c40324c11bbc232133fc7a
phuc <phuc@DESKTOP-VOAB83U>
2026-07-19 11:47:28 +0700

    lab01

---
+++ ShopFruit/Core.Database/Interfaces/Meta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Database.Interfaces
+{
+    public interface Meta
+    {
+        public string? Keywork { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ ShopFruit/Core.Database/Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Database.Models
+{
+    [Table("Group")]
+    public class Group
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string? Name { get; set; }
+        public ICollection<Member> Member { get; set; } = new HashSet<Member>();
+        public ICollection<Autherized> Autherized { get; set; } = new HashSet<Autherized>();
+    }
+}
